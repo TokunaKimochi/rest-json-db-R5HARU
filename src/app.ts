a05394a649f002666116a4db83b0f485c4ba7e54
TokunaKimochi <tokunakimochi@gmail.com>
2023-06-18 18:44:00 +0900

    ミドルウェアにエラーハンドリングを定義

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express, { Express, Response } from 'express';
+import cors from 'cors';
+import MessageResponse from 'interfaces/MessageResponse';
+import * as middleWares from './middleWares';
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (_, res: Response) => {
+app.get('/', (_, res: Response<MessageResponse>) => {
   res.json({
     message: '☕Hallo Local Area Network❢',
   });
 });
+
+app.use(middleWares.notFound);
+app.use(middleWares.errorHandler);
 
 export default app;
---
+++ src/middleWares.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import ErrorResponse from 'interfaces/ErrorResponse';
+
+class HttpException extends Error {
+  statusCode?: number;
+
+  message: string;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.message = message;
+  }
+}
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  res.status(404);
+  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+  next(error);
+}
+
+export function errorHandler(err: HttpException, _: Request, res: Response<ErrorResponse>) {
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
