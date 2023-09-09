ed658a3cb4b1013f85d53fe2db193c1121a47213
TokunaKimochi <tokunakimochi@gmail.com>
2023-09-09 09:30:36 +0900

    型修正

--- src/private-sql-functions/findMaxSha1SameVal.ts
+++ src/private-sql-functions/findMaxSha1SameVal.ts
@@ -1,6 +1,6 @@
 import { DataBaseError, db } from '../db';
 
-const findMaxSha1SameVal = async (sha1: string): Promise<{ max_same_val: string | null }> => {
+const findMaxSha1SameVal = async (sha1: string): Promise<{ max_same_val: number | null }> => {
   const result = await db
     .one(`SELECT MAX(sha1_same_val) AS "max_same_val" FROM customers WHERE address_sha1 = '${sha1}'`)
     .catch((err: Error) => Promise.reject(new DataBaseError(err)));
