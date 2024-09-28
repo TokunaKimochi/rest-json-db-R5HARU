3e8f8776a8540fd54c675611e1aa00ce2ee7972d
TokunaKimochi <tokunakimochi@gmail.com>
2024-09-28 13:07:34 +0900

    ejpc-generate-json のラッパーがとりあえず完成

--- src/scripts/ejPostalApiUpdate.ts
+++ src/scripts/ejPostalApiUpdate.ts
@@ -1,24 +1,37 @@
 import { spawn } from 'node:child_process';
+import symlinkDir from 'symlink-dir';
 
-const hoo = () =>
+const date = new Date().toLocaleString('sv', { timeZone: 'Asia/Tokyo' }).replace(/\D/g, '.');
+const targetPath = `./vendor/easy-ja-postal-code/${date}`;
+
+const runEjpcGenerateJson = () =>
   new Promise<void>((resolve) => {
-    const date = new Date().toLocaleString('sv', { timeZone: 'Asia/Tokyo' }).replace(/\D/g, '.');
-    const targetPath = `./vendor/easy-ja-postal-code/${date}`;
     // 依存関係の cli を pnpm run。`pnpm exec` だと npm scripts と関係なしに呼び出せる
     const subProcess = spawn('pnpm', ['exec', 'ejpc-generate-json', '-d', targetPath, '-t'], {
       stdio: ['inherit', 'pipe', 'inherit'],
       shell: true,
     });
-    if (!subProcess) console.log('⚠️予期しない動作を検出しました！');
+    if (!subProcess) console.error('⚠️予期しない動作を検出しました！');
+    process.stdout.write('Downloading and JSON generating.');
     subProcess.stdout.on('data', (data) => {
-      if (typeof data !== 'object') console.log('⚠️予期しない動作を検出しました！');
-      console.log(`ℹ️${data}`);
+      if (typeof data !== 'object') console.error('⚠️予期しない動作を検出しました！');
+      process.stdout.write('.');
     });
-    subProcess.on('close', () => resolve());
+    subProcess.on('close', () => {
+      process.stdout.write('\nDone.\n');
+      resolve();
+    });
   });
 async function main() {
-  await hoo();
+  await runEjpcGenerateJson();
   console.log('●工程1/2: 成功✨ JSON を作成しました');
+
+  const resultObj = (await symlinkDir(targetPath, './vendor/easy-ja-postal-code/api').catch((e: string) =>
+    console.error(`  >>>> ✘ ${e}`)
+  )) || { warn: '' };
+  if (resultObj.warn) throw new Error(`symlinkDir().warn: ${resultObj.warn}`);
+
+  console.log('●工程2/2: 成功✨ api にリンクを通しました');
 }
 
 try {
