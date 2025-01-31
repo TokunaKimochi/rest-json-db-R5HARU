e8963cee32121d92bff66bbd6f94f5b79f79e457
yorocomv <ishihara@gyokuroen.co.jp>
2025-01-31 17:20:24 +0900

    2025年1月第5週 作業分

--- src/routes/shipping-instruction-printouts/shippingInstructionPrintouts.schemas.ts
+++ src/routes/shipping-instruction-printouts/shippingInstructionPrintouts.schemas.ts
@@ -1,30 +1,34 @@
 import { z } from 'zod';
 
-const shippingInstructionPrintHistoryTbRowSchema = z
-  .object({
-    delivery_date: z.coerce
-      .date()
-      .transform((val) => val.toLocaleString('sv-SE', { timeZone: 'Asia/Tokyo', dateStyle: 'short' })),
-    delivery_time_str: z.string().max(32),
-    printed_at: z.string().datetime({ offset: true }).optional(),
-    page_num_str: z.string().max(8),
-    customer_name: z.string().max(60),
-    customer_address: z.string().max(96),
-    wholesaler: z.string().max(32),
-    order_number: z.string().max(64),
-    shipping_date: z.union([
-      // ただし、DB側はDATE型で固定なのでデフォルトになるように
-      // shipping_date 自体を削る処理をする
-      z.string().length(0),
-      z.coerce
-        .date()
-        .transform((val) => val.toLocaleString('sv-SE', { timeZone: 'Asia/Tokyo', dateStyle: 'short' }))
-        .optional(),
-    ]),
-    carrier: z.string().max(32),
-    package_count: z.coerce.number().int().nonnegative(),
-    items_of_order: z.string(),
-  })
-  .brand<'ShippingInstructionPrintHistoryTbRow'>();
+export const shippingInstructionPrintHistoryInputSchema = z.object({
+  delivery_date: z.coerce
+    .date()
+    .transform((val) => val.toLocaleString('sv-SE', { timeZone: 'Asia/Tokyo', dateStyle: 'short' })),
+  delivery_time_str: z.string().max(32),
+  // タイムスタンプ with Timezone
+  printed_at: z.string().datetime({ offset: true }).optional(),
+  page_num_str: z.string().max(8),
+  customer_name: z.string().max(60),
+  customer_address: z.string().max(96),
+  wholesaler: z.string().max(32),
+  order_number: z.string().max(64),
+  shipping_date: z.union([
+    // ただし、DB側はDATE型で固定なのでデフォルトになるように
+    // shipping_date 自体を削る処理をする
+    z.string().length(0),
+    z.coerce.date().transform((val) => val.toLocaleString('sv-SE', { timeZone: 'Asia/Tokyo', dateStyle: 'short' })),
+  ]),
+  carrier: z.string().max(32),
+  package_count: z.coerce.number().int().nonnegative().optional(),
+  items_of_order: z.string(),
+});
 
-export default shippingInstructionPrintHistoryTbRowSchema;
+export const shippingInstructionPrintHistoryInputWithOptionalShippingDateSchema =
+  shippingInstructionPrintHistoryInputSchema.partial({ shipping_date: true });
+
+export const shippingInstructionPrintHistoryTbRowSchema = shippingInstructionPrintHistoryInputSchema.required().extend({
+  shipping_date: z.coerce
+    .date()
+    .transform((val) => val.toLocaleString('sv-SE', { timeZone: 'Asia/Tokyo', dateStyle: 'short' })),
+  package_count: z.coerce.number().int().nonnegative().nullable(),
+});
