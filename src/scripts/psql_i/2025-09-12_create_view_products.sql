cc59c79d777bd282a4dadebaa21fdf34afbb56dc
yorocomv <ishihara@gyokuroen.co.jp>
2025-11-21 17:21:29 +0900

    セット商品候補リスト・ビュー

--- src/scripts/psql_i/2025-09-12_create_view_products.sql
+++ src/scripts/psql_i/2025-09-12_create_view_products.sql
@@ -109,3 +109,72 @@ FROM
     JOIN products p ON pcmp.product_id = p.id
     LEFT JOIN unit_types ut ON pcmp.unit_type_id = ut.id
     LEFT JOIN product_inner_packaging_types ipt ON pcmp.inner_packaging_type_id = ipt.id;
+
+-- セット品や SKU を作る時のアイテム候補一覧ビュー
+CREATE OR REPLACE VIEW v_single_products AS
+SELECT
+    -- Product (単体商品)
+    p.id AS product_id,
+    p.name AS product_name,
+    p.short_name AS product_short_name,
+    p.internal_code,
+    p.available_date,
+    p.discontinued_date,
+    p.depth_mm,
+    p.width_mm,
+    p.diameter_mm,
+    p.height_mm,
+    p.weight_g,
+    p.note AS product_note,
+    p.ulid_str,
+    -- Basic Product
+    bp.name AS basic_product_name,
+    bp.jan_code,
+    bp.predecessor_id,
+    bp.expiration_value,
+    bp.expiration_unit,
+    pst.name AS sourcing_type,
+    pc.name AS category_name,
+    ppt.name AS packaging_type,
+    -- Supplier
+    -- name2がNOT NULL DEFAULT '' のため、RTRIMでname2が空文字の場合の余分な末尾スペースを削除
+    RTRIM(s.name1 || ' ' || s.name2) AS supplier_name,
+    -- First Component (代表成分・内容量)
+    pcmp.title AS component_title,
+    pcmp.symbol AS component_symbol,
+    pcmp.amount AS component_amount,
+    ut.name AS component_unit_name,
+    pcmp.pieces AS component_pieces,
+    ipt.name AS component_inner_packaging_type
+FROM
+    products p
+    JOIN basic_products bp ON p.basic_id = bp.id
+    LEFT JOIN suppliers s ON p.supplier_id = s.id
+    LEFT JOIN product_sourcing_types pst ON bp.sourcing_type_id = pst.id
+    LEFT JOIN product_categories pc ON bp.category_id = pc.id
+    LEFT JOIN product_packaging_types ppt ON bp.packaging_type_id = ppt.id
+    -- LATERAL JOIN: 各製品(p)に紐づく最初の1件の成分(pcmp)を効率的に取得
+    LEFT JOIN LATERAL (
+        SELECT
+            pcmp.title,
+            pcmp.symbol,
+            pcmp.amount,
+            pcmp.unit_type_id,
+            pcmp.pieces,
+            pcmp.inner_packaging_type_id
+        FROM
+            product_components pcmp
+        WHERE
+            -- LATERAL (横方向) によって外側のテーブルを参照
+            pcmp.product_id = p.id
+        ORDER BY
+            pcmp.id ASC
+        LIMIT
+            1
+            -- サブクエリの WHERE句で結合条件が既出 -> 常に結合 TRUE
+            -- サブクエリの結果が無ければ LEFT JOIN で NULL を代入
+    ) pcmp ON TRUE
+    LEFT JOIN unit_types ut ON pcmp.unit_type_id = ut.id
+    LEFT JOIN product_inner_packaging_types ipt ON pcmp.inner_packaging_type_id = ipt.id
+WHERE
+    p.is_set_product = false;
